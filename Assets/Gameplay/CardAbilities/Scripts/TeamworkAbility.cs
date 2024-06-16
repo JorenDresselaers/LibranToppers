b7824d87b33968458d67247a3b162ce8023af1a5
JorenDresselaers <99868033+JorenDresselaers@users.noreply.github.com>
2024-06-16 18:29:46 +0200

    Added Bartolomeo

--- Assets/Gameplay/CardAbilities/Scripts/TeamworkAbility.cs
+++ Assets/Gameplay/CardAbilities/Scripts/TeamworkAbility.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 [Serializable, CreateAssetMenu(menuName = "Abilities/Teamwork", fileName = "TeamworkAbility")]
 public class TeamworkAbility : CardAbility
 {
+    [SerializeField] private Mode _mode = Mode.NAME;
     [SerializeField] private List<string> _teamworkNames = new();
+    [SerializeField] private CardData.Faction _teamworkFaction = CardData.Faction.None;
     [SerializeField] private int _assaultBuff = 0;
     [SerializeField] private int _vitalityBuff = 0;
     [SerializeField] private bool _buffSelf = true;
     [SerializeField] private bool _canStack = true;
+
+    enum Mode
+    { 
+        FACTION,
+        NAME
+    }
 
     private void Awake()
     {
@@ -31,21 +39,45 @@ public class TeamworkAbility : CardAbility
     {
         foreach(Card card in caster._player.Board.Cards)
         {
-            if (_teamworkNames.Contains(card.CardName))
+            if (_mode == Mode.NAME)
             {
-                if (_buffSelf)
+                if (_teamworkNames.Contains(card.CardName))
                 {
-                    caster.ModifyStats(_assaultBuff, _vitalityBuff);
-                    Debug.Log($"Stats of {caster.CardName} changed by +{_vitalityBuff}/+{_assaultBuff}");
+                    if (_buffSelf)
+                    {
+                        caster.ModifyStats(_assaultBuff, _vitalityBuff);
+                        Debug.Log($"Stats of {caster.CardName} changed by +{_vitalityBuff}/+{_assaultBuff}");
+                    }
+                    else
+                    {
+                        card.ModifyStats(_assaultBuff, _vitalityBuff);
+                        Debug.Log($"Stats of {card.CardName} changed by +{_vitalityBuff}/+{_assaultBuff}");
+
+                    }
+
+                    if (!_canStack) return;
                 }
-                else
+            }
+            else if(_mode == Mode.FACTION)
+            {
+                if (card.Faction == _teamworkFaction)
                 {
-                    card.ModifyStats(_assaultBuff, _vitalityBuff);
-                    Debug.Log($"Stats of {card.CardName} changed by +{_vitalityBuff}/+{_assaultBuff}");
+                    if (_buffSelf)
+                    {
+                        caster.ModifyStats(_assaultBuff, _vitalityBuff);
+                        Debug.Log($"Stats of {caster.CardName} changed by +{_vitalityBuff}/+{_assaultBuff}");
+                    }
+                    else
+                    {
+                        if (card != caster)
+                        {
+                            card.ModifyStats(_assaultBuff, _vitalityBuff);
+                            Debug.Log($"Stats of {card.CardName} changed by +{_vitalityBuff}/+{_assaultBuff}");
+                        }
+                    }
 
+                    if (!_canStack) return;
                 }
-
-                if(!_canStack) return;
             }
         }
     }
