4d610078d4718c9bc26b752bdce008a3cbd9b8ba
JorenDresselaers <99868033+JorenDresselaers@users.noreply.github.com>
2024-06-16 14:52:42 +0200

    Added Relay for online multiplayer

--- Assets/Gameplay/Network/LibranToppersNetworkManager.cs
+++ Assets/Gameplay/Network/LibranToppersNetworkManager.cs
@@ -2,6 +2,11 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
+using System.Collections.Generic;
+using Unity.Services.Relay.Models;
+using Utp;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
 
 /*
 	Documentation: https://mirror-networking.gitbook.io/docs/components/network-manager
@@ -14,14 +19,145 @@ public class LibranToppersNetworkManager : NetworkManager
     // have to cast to this type everywhere.
     public static new LibranToppersNetworkManager singleton => (LibranToppersNetworkManager)NetworkManager.singleton;
 
+    private UtpTransport utpTransport;
+
     /// <summary>
-    /// Runs on both Server and Client
-    /// Networking is NOT initialized when this fires
+    /// Server's join code if using Relay.
     /// </summary>
+    public string relayJoinCode = "";
+
     public override void Awake()
     {
         base.Awake();
+
+        utpTransport = GetComponent<UtpTransport>();
+
+        string[] args = System.Environment.GetCommandLineArgs();
+        for (int key = 0; key < args.Length; key++)
+        {
+            if (args[key] == "-port")
+            {
+                if (key + 1 < args.Length)
+                {
+                    string value = args[key + 1];
+
+                    try
+                    {
+                        utpTransport.Port = ushort.Parse(value);
+                    }
+                    catch
+                    {
+                        UtpLog.Warning($"Unable to parse {value} into transport Port");
+                    }
+                }
+            }
+        }
+
+        InitializeRelay();
     }
+
+    private async void InitializeRelay()
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            //If not already logged, log the user in
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+        print("Relay initialized");
+    }
+
+    #region Relay
+    /// <summary>
+    /// Get the port the server is listening on.
+    /// </summary>
+    /// <returns>The port.</returns>
+    public ushort GetPort()
+    {
+        return utpTransport.Port;
+    }
+
+    /// <summary>
+    /// Get whether Relay is enabled or not.
+    /// </summary>
+    /// <returns>True if enabled, false otherwise.</returns>
+    public bool IsRelayEnabled()
+    {
+        return utpTransport.useRelay;
+    }
+
+    /// <summary>
+    /// Ensures Relay is disabled. Starts the server, listening for incoming connections.
+    /// </summary>
+    public void StartStandardServer()
+    {
+        utpTransport.useRelay = false;
+        StartServer();
+    }
+
+    /// <summary>
+    /// Ensures Relay is disabled. Starts a network "host" - a server and client in the same application
+    /// </summary>
+    public void StartStandardHost()
+    {
+        utpTransport.useRelay = false;
+        StartHost();
+    }
+
+    /// <summary>
+    /// Gets available Relay regions.
+    /// </summary>
+    /// 
+    public void GetRelayRegions(Action<List<Region>> onSuccess, Action onFailure)
+    {
+        utpTransport.GetRelayRegions(onSuccess, onFailure);
+    }
+
+    /// <summary>
+    /// Ensures Relay is enabled. Starts a network "host" - a server and client in the same application
+    /// </summary>
+    public void StartRelayHost(int maxPlayers, string regionId = null)
+    {
+        utpTransport.useRelay = true;
+        utpTransport.AllocateRelayServer(maxPlayers, regionId,
+        (string joinCode) =>
+        {
+            relayJoinCode = joinCode;
+
+            StartHost();
+        },
+        () =>
+        {
+            UtpLog.Error($"Failed to start a Relay host.");
+        });
+    }
+
+    /// <summary>
+    /// Ensures Relay is disabled. Starts the client, connects it to the server with networkAddress.
+    /// </summary>
+    public void JoinStandardServer()
+    {
+        utpTransport.useRelay = false;
+        StartClient();
+    }
+
+    /// <summary>
+    /// Ensures Relay is enabled. Starts the client, connects to the server with the relayJoinCode.
+    /// </summary>
+    public void JoinRelayServer()
+    {
+        utpTransport.useRelay = true;
+        utpTransport.ConfigureClientWithJoinCode(relayJoinCode,
+        () =>
+        {
+            StartClient();
+        },
+        () =>
+        {
+            UtpLog.Error($"Failed to join Relay server.");
+        });
+    }
+    #endregion
 
     #region Unity Callbacks
 
--- Assets/Gameplay/Network/StartMirrorServer.cs
+++ Assets/Gameplay/Network/StartMirrorServer.cs
@@ -1,29 +1,71 @@
 using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
+using TMPro;
+using System.Collections;
 
 public class StartMirrorServer : MonoBehaviour
 {
-    public InputField _ipAddressInput; // Input field for entering server IP address
+    public TMP_InputField _joinCodeInput;
+    public TMP_InputField _ipAddressInput;
     [SerializeField] private string _sceneName;
+    public bool _useRelay = true;
+
+    private void Awake()
+    {
+        if(!_useRelay)
+        {
+            _joinCodeInput.gameObject.SetActive(false);
+        }
+    }
 
     public void StartServer()
     {
         // Start the Mirror server if it's not already running
         if (!NetworkServer.active)
         {
-            NetworkManager.singleton.StartHost();
+            LibranToppersNetworkManager manager = NetworkManager.singleton as LibranToppersNetworkManager;
+
+            if (_useRelay)
+            {
+                manager.StartRelayHost(2);
+                StartCoroutine(SetCodeWhenAvailable());
+            }
+            else
+            {
+                NetworkManager.singleton.StartHost();
+            }
+
             APIManager.Instance?.ToggleUI(false);
         }
+    }
+
+    private IEnumerator SetCodeWhenAvailable()
+    {
+        LibranToppersNetworkManager manager = NetworkManager.singleton as LibranToppersNetworkManager;
+        yield return new WaitUntil(() => manager.relayJoinCode != "");
+        _joinCodeInput.text = manager.relayJoinCode;
     }
 
     public void JoinServerAsClient()
     {
         // Join the server as a client
-        string networkAddress = "127.0.0.1";
-        if (_ipAddressInput != null && _ipAddressInput.text != "") networkAddress = _ipAddressInput.text;
-        NetworkManager.singleton.networkAddress = networkAddress;
-        NetworkManager.singleton.StartClient();
+        if (_useRelay)
+        {
+            LibranToppersNetworkManager manager = NetworkManager.singleton as LibranToppersNetworkManager;
+            if(_joinCodeInput.text != "")
+            {
+                manager.relayJoinCode = _joinCodeInput.text;
+                manager.JoinRelayServer();
+            }
+        }
+        else
+        {
+            string networkAddress = "127.0.0.1";
+            if (_ipAddressInput != null && _ipAddressInput.text != "") networkAddress = _ipAddressInput.text;
+            NetworkManager.singleton.networkAddress = networkAddress;
+            NetworkManager.singleton.StartClient();
+        }
         APIManager.Instance?.ToggleUI(false);
     }
 }
